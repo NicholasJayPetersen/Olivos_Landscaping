3058e1eee49329dcdf79a3c7c035bbffc1b65b9a
Nicholas Petersen <nicholas.jay.petersen@gmail.com>
2026-07-20 20:20:46 -0400

    Add images

--- site/js/nav.js
+++ site/js/nav.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <nav>
                 <div id="logo">
                     <!--temporary placeholder for logo-->
-                    <img src="https://imgs.search.brave.com/3U5LYlXfgNQo5WCvc8Do-1VKBKsRWWN5tGtIBr1bD0w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4z/LmVtb2ppLmdnL2Vt/b2ppcy8yNzk3X3do/aXRlX2NpcmNsZS5w/bmc" alt="Olivos Landscaping Logo" width="75" height="75">
+                    <img src="./images/logo-transparent.png" alt="Olivos Landscaping Logo" width="75" height="75">
                 </div>
                 <ul>
                     <li><a href="./index.html">Home</a></li>
