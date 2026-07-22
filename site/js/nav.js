0d088646e28bef83cb5f1f9a055fd0003b63fb6c
Nicholas Petersen <nicholas.jay.petersen@gmail.com>
2026-07-21 22:02:42 -0400

    add quote modal

--- site/js/nav.js
+++ site/js/nav.js
@@ -8,13 +8,42 @@ document.addEventListener('DOMContentLoaded', function() {
                         <p>Olivo's Landscaping</p>
                     </div>
                 </a>
+                <button id="quote-button" class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Quote</button>
                 <ul>
                     <li><a href="./index.html">Home</a></li>
                     <li><a href="./gallery.html#Gallery">Gallery</a></li>
                     <li><a href="./services.html#Services">Services</a></li>
                     <li><a href="./contact.html#contact">Contact</a></li>
                 </ul>
-                <button id="quote-modal">Quote</button>
             </nav>
-            `;
+        `;
+
+    const modal = document.querySelector("#modal");
+    modal.innerHTML = `
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Request a Quote</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post">
+                        <for id="name">Name:</for>
+                        <input type="text" id="name" name="name" required>
+                        <for id="email">email:</for>
+                        <input type="email" id="email" name="email" required>
+                        <for id="phone">Phone:</for>
+                        <input type="tel" id="phone" name="phone" required>
+                        <textarea id="message" name="message" placeholder="How can we help?" width="200px" height="100px" required></textarea>
+                        <button type="button" class="btn btn-primary">Send</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    `;
 });
