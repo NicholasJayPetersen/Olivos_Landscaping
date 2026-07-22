5b2aac0aaf3480d2d0162245e0ca21d0689f8be5
Nicholas Petersen <nicholas.jay.petersen@gmail.com>
2026-07-21 20:40:33 -0400

    homepage styling

--- site/js/nav.js
+++ site/js/nav.js
@@ -2,15 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
     header.innerHTML = `
             <nav>
-                <div id="logo">
-                    <a href="./index.html"><img src="./images/logo-transparent.png" alt="Olivos Landscaping Logo" width="75" height="75"></a>
-                </div>
+                <a href="./index.html">
+                    <div id="logo">
+                        <img src="./images/logo-transparent.png" alt="Olivos Landscaping Logo" width="75" height="75">
+                        <p>Olivo's Landscaping</p>
+                    </div>
+                </a>
                 <ul>
                     <li><a href="./index.html">Home</a></li>
                     <li><a href="./gallery.html#Gallery">Gallery</a></li>
                     <li><a href="./services.html#Services">Services</a></li>
                     <li><a href="./contact.html#contact">Contact</a></li>
                 </ul>
+                <button id="quote-modal">Quote</button>
             </nav>
             `;
 });
